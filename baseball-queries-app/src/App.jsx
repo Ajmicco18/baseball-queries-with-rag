dc079f4634c2679430a75f561992b705ea251955
Anthony Micco <anthonymicco2003@gmail.com>
2025-10-08 20:07:34 -0400

    initalized frontend and backend folders

---
+++ baseball-queries-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center", color: "#D50032" }}>Baseball Queries Website</h1>
+    </>
+  )
+}
+
+export default App
