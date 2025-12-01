23b8b0c34589f54ac6b214657e78414761b2f9b6
Anthony Micco <anthonymicco2003@gmail.com>
2025-12-01 13:55:38 -0500

    fixed CORS issue when calling backend endpoint from frontend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
 from ragModel.rag_model import generate_model
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
+# defining FastAPI application
 app = FastAPI()
+
+# setting our frontend origin
+origins = ["http://localhost:5173"]
+
+# setting permissions to allow for cross-origin communication
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class ChatQuery(BaseModel):
+    query: str
+
+# endpoint to read in the API's root
 
 
 @app.get("/")
@@ -10,7 +31,10 @@ def read_root():
     return {"Hello": "World"}
 
 
+# endpoint to receive the response from the RAG model
+
+
 @app.post("/chat")
-def generate_respone(query: str):
-    response = generate_model(query)
+async def generate_respone(req: ChatQuery):
+    response = generate_model(req.query)
     return {"Response": response}
--- baseball-queries-app/src/App.jsx
+++ baseball-queries-app/src/App.jsx
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 //Figure out how to get API URL in .env
-//const BASE_URL = process.env.API_URL
+const BASE_URL = import.meta.env.VITE_API_URL
 
 function App() {
 
@@ -14,17 +14,21 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post('http://127.0.0.1:8000/chat', {
+      const response = await axios.post(`${BASE_URL}/chat`, {
         query: query
       })
       console.log(response.data)
       setIsResponse(true)
     }
-    catch {
+    catch (error) {
       console.error("Error: ", error)
     }
 
     setQuery("")
+  }
+
+  const checkURL = () => {
+    console.log(BASE_URL)
   }
 
   return (
@@ -38,6 +42,7 @@ function App() {
 
         </TextField>
         <Button onClick={handleSubmit} sx={{ mt: "5px", backgroundColor: "white", color: "#D50032", "&:hover": { backgroundColor: "#D50032", color: "white" } }}>Query</Button>
+        <Button onClick={checkURL} sx={{ mt: "5px", backgroundColor: "white", color: "#D50032", "&:hover": { backgroundColor: "#D50032", color: "white" } }}>Check</Button>
       </Box>
 
     </>
